8d5ca5e26eab96dffe6399fbcf2b6d499c48e024
jackypan1989 <jackypan1989@gmail.com>
2017-11-01 18:01:52 +0800

    add textinput

--- app/Main.elm
+++ app/Main.elm
@@ -6,31 +6,58 @@ import NativeUi.Style as Style exposing (..)
 import NativeUi.Events exposing (..)
 import NativeUi.Elements as Elements exposing (..)
 
-type alias Model = String
-type Msg = NoOp
+type alias Model = {
+  userInput: String
+}
+type Msg = NoOp | ChangeSearchInput String
 
 init : ( Model, Cmd Msg) 
 init = 
-  ( "hello", Cmd.none )
+  ( { userInput = "香蕉" } , Cmd.none )
 
 view : Model -> Node Msg
-view model = Elements.view [] (mainView model)
+view model = 
+  Elements.view [
+    Ui.style [
+      Style.paddingTop 30
+    ]
+  ] (mainView model)
 
 mainView : Model -> List (Node Msg)
 mainView model =
-    [Elements.view [] [
-        text [] [Ui.string "Hello World"],
-        button NoOp "#666" "PressMe"
-    ]]
-
---   [ text [] [ Ui.string "Hello World!" ]
---   , text [] [ Ui.string "Hello World! Again~" ]
---   ]
+  [ Elements.view [] [
+    text [
+      Ui.style [
+        Style.fontSize 30,
+        Style.textAlign "center"
+      ]
+    ] [ Ui.string "水果價格搜索" ],
+    text [
+      Ui.style [
+        Style.fontSize 30,
+        Style.textAlign "center"
+      ]
+    ] [ Ui.string model.userInput ],
+    Elements.textInput
+      [ Ui.style 
+          [ Style.height 28
+          , Style.backgroundColor "#444444"
+          , Style.marginTop 8
+          , Style.color "white"
+          , Style.padding 4
+          , Style.borderRadius 4
+          ] 
+      , onChangeText ChangeSearchInput
+      ]
+      [] ,
+    button NoOp "grey" "搜索"
+  ] ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
   case msg of
     NoOp -> ( model, Cmd.none )
+    ChangeSearchInput input -> ( { model | userInput = input }, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
@@ -46,21 +73,19 @@ main =
 
 button : Msg -> String -> String -> Node Msg
 button msg color content =
-    text
-        [ Ui.style
-            [ Style.color "white"
-            , Style.textAlign "center"
-            , Style.backgroundColor color
-            , Style.paddingTop 5
-            , Style.paddingBottom 5
-            , Style.width 30
-            , Style.fontWeight "bold"
-            , Style.shadowColor "#000"
-            , Style.shadowOpacity 0.25
-            , Style.shadowOffset 1 1
-            , Style.shadowRadius 5
-            , Style.transform { defaultTransform | rotate = Just "10deg" }
-            ]
-        , onPress msg
-        ]
-        [ Ui.string content ]
+  text
+    [ Ui.style
+      [ Style.color "white"
+      , Style.textAlign "center"
+      , Style.backgroundColor color 
+      , Style.width 100
+      , Style.height 40
+      , Style.fontSize 20
+      , Style.paddingTop 10
+      , Style.marginVertical 30
+      , Style.marginHorizontal 160
+      , Style.fontWeight "bold"
+      ]
+      , onPress msg
+    ]
+    [ Ui.string content ]
