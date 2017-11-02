aa93dd469304960fe8fac54f3b251384270bd6fa
YinTing.Tan <vr2006ccc@gmail.com>
2017-11-02 18:17:06 +0800

    Add elm-date-format

--- src/Web.elm
+++ src/Web.elm
@@ -7,19 +7,26 @@ import Model exposing (..)
 import Update exposing (..)
 import Message exposing (Msg(..))
 import Subscription exposing (..)
-
+import Date.Format exposing (format)
+import Date
 -- VIEW
 view : Model -> Html Msg
 view model = div [] 
-    [ div [] [ text "果菜市價查詢" ]
-    , input [ placeholder "請輸入蔬果名", onInput ChangeSearchInput ] []
-    , button [ onClick ClickSearch ] [ text "搜索" ]
-    , div [ style [( "width", "800px" ), ( "display", "flex" ), ( "flex-wrap", "wrap" )]]
-        [ row [ "交易日期", "作物名稱", "市場名稱", "平均價" ]
-        , div [] 
-            ( List.map (\info -> row [info.tradeDate, info.cropName, info.marketName, toString (round (info.avgPrice * 0.6))]) model.data )
+        [ div [] [ text "果菜市價查詢" ]
+        , input [ placeholder "請輸入蔬果名", onInput ChangeSearchInput ] []
+        , button [ onClick ClickSearch ] [ text "搜索" ]
+        , div [ style [( "width", "800px" ), ( "display", "flex" ), ( "flex-wrap", "wrap" )]]
+            [ row [ "交易日期", "作物名稱", "市場名稱", "平均價" ]
+            , div [] 
+                ( List.map (\info -> 
+                    let date =
+                        case Date.fromString info.tradeDate of
+                            Ok d -> format "%Y%m%d" d
+                            Err e -> ""
+                    in
+                    row [date, info.cropName, info.marketName, toString (round (info.avgPrice * 0.6))]) model.data )
+            ]
         ]
-    ]
 
 row : List String -> Html Msg
 row titles =
