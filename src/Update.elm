d750108e554b8e8df900426c11073252f443f614
jackypan1989 <jackypan1989@gmail.com>
2017-11-02 16:45:19 +0800

    add nested decoder

--- src/Update.elm
+++ src/Update.elm
@@ -20,23 +20,32 @@ update msg model =
 
 getPriceData : String -> Http.Request Response
 getPriceData query =
-    Http.get (baseUrl ++ "cropName=" ++ query) decodePriceData
+    Http.get (baseUrl ++ "cropName=" ++ query) decodeResponse
 
-decodePriceData: Decode.Decoder Response
-decodePriceData = 
+decodeResponse : Decode.Decoder Response
+decodeResponse =
     decode Response
+        |> required "docs" (Decode.list decodePriceData)
+        |> required "total" (Decode.int)
+        |> required "limit" (Decode.int)
+        |> required "page" (Decode.int)
+        |> required "pages" (Decode.int)
+
+decodePriceData : Decode.Decoder Info
+decodePriceData = 
+    decode Info
         |> required "tradeDate" (Decode.string)
         |> required "cropCode" (Decode.string)
         |> required "cropName" (Decode.string)
         |> required "marketCode" (Decode.string)
         |> required "marketName" (Decode.string)
-        |> required "highPrice" (Decode.int)
-        |> required "middlePrice" (Decode.int)
-        |> required "lowPrice" (Decode.int)
-        |> required "avgPrice" (Decode.int)
+        |> required "highPrice" (Decode.float)
+        |> required "middlePrice" (Decode.float)
+        |> required "lowPrice" (Decode.float)
+        |> required "avgPrice" (Decode.float)
         |> required "tradeCount" (Decode.int)
         |> required "updated" (Decode.string)
         |> required "created" (Decode.string)
 
-sendRequest: String -> Cmd Msg
+sendRequest : String -> Cmd Msg
 sendRequest query = Http.send NewPriceData (getPriceData query)
